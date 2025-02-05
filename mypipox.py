1bb21d8fe60d701d0dd929ab93ef0d4427123be1
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-05 14:23:29 +0900

    Update mypipox.py

--- mypipox.py
+++ mypipox.py
@@ -1,6 +1,9 @@
+#!/usr/env/python3 python
+
 def main():
 	p = input()
 	while p:
 		print(f"pip {{'D':'download','X':'install'}[p[0]]} -r {p[1:].replace('E:', ' --extra-index-url=').replace('F:', ' --find-links=').replace('I:', ' --i=').replace('N', ' --no-index')}")
 		p = input()
+
 if __name__=="__main__":main()
