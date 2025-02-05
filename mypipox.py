d2ab5836d2c79ae795af8030ea2e6e3b16f54083
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-05 14:20:09 +0900

    Add files via upload

---
+++ mypipox.py
@@ -0,0 +1,6 @@
+def main():
+	p = input()
+	while p:
+		print(f"pip {{'D':'download','X':'install'}[p[0]]} -r {p[1:].replace('E:', ' --extra-index-url=').replace('F:', ' --find-links=').replace('I:', ' --i=').replace('N', ' --no-index')}")
+		p = input()
+if __name__=="__main__":main()
